fcbec96572bacde6d7ac1f2514132674d3de0713
Rodrigo Cibils <11689014+rodcibils@users.noreply.github.com>
2025-05-16 12:13:41 -0300

    Added console log for generated seed

--- api/src/index.ts
+++ api/src/index.ts
@@ -19,6 +19,7 @@ app.get('/seed', (_req, res) => {
   const seed = crypto.randomBytes(16).toString('hex'); // 32-character hex string
   const expiresAt = new Date(Date.now() + 5 * 60 * 1000).toISOString(); // expires in 5 minutes
   const response: SeedResponse = { seed, expires_at: expiresAt };
+  console.log(`Generated Seed: ${JSON.stringify(response)}`);
 
   res.status(200).json(response);
 });
